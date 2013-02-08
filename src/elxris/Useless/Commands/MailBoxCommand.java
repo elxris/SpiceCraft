861e4188fec8066dd67e7478f0aaa251d47c7ed8
elxris <cristiancehi@gmail.com>
2013-02-08 17:31:44 -0600

    0.8.5.3.1
    - Preparando el comando mbox y mboxc para juntarlos.

--- src/elxris/Useless/Commands/MailBoxCommand.java
+++ src/elxris/Useless/Commands/MailBoxCommand.java
@@ -1,5 +1,8 @@
 package elxris.Useless.Commands;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -39,6 +42,58 @@ public class MailBoxCommand extends Comando{
             mensaje(jugador, "mbox.deleted");
         }
         mail.interpreta();
+        
+        
+        
+        if(!jugador.hasPermission("useless.mail")){
+            return true;
+        }
+        
+        if(isCommand("comm.mail.new", args[0])){
+            if(args.length > 1){
+                mail.createBorrador(jugador.getName(), cortarArray(1, args));              
+            }
+        }else if(isCommand("comm.mail.add", args[0])){
+            if(args.length > 1){
+                mail.addMensaje(jugador.getName(), getString(cortarArray(1, args)));
+            }
+        }else if(isCommand("comm.mboxc.send", args[0])){
+            if(args.length == 1){
+                mail.sendMensaje(jugador.getName());                
+            }else if(args.length >= 3){
+                List<String> destinatario = new ArrayList<String>();
+                destinatario.add(args[1]);
+                mail.sendMensaje(jugador.getName(), destinatario, getString(cortarArray(2, args)), false);
+            }
+            return true;
+        }else if(isCommand("comm.mboxc.sendall", args[0])){
+            if(args.length == 1){
+                mail.sendMensajeATodos(jugador.getName());
+            }else if(args.length > 1){
+                String mess = getString(cortarArray(1, args));
+                mail.sendMensajeATodos(jugador.getName(), mess);
+            }
+        }else{
+            mensaje(jugador, "mboxc.info");
+            return true;
+        }
+        mail.interpreta();
         return true;
+    }
+    public String[] cortarArray(int id, String[] args){
+        List<String> resultado = new ArrayList<String>();
+        for(int i = id; i < args.length; i++){
+            resultado.add(args[i]);
+        }
+        return resultado.toArray(new String[resultado.size()]);
+    }
+    
+    public String getString(String[] args){
+        String string = "";
+        for(String k: args){
+            string = string.concat(k);
+            string = string.concat(" ");
+        }
+        return string;
     }
 }
