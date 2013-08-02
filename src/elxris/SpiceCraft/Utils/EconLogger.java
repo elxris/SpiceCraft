41a1ecc33d99875e2b73b08ecb3ac4ba51af4ed1
elxris <cristiancehi@gmail.com>
2013-08-02 17:11:49 -0500

    [0.9.3.6b] Config logg.
    [^]Bugfix [!]Updated [+]Feature [-]Removed
    
    + Config logg desactivable.

--- src/elxris/SpiceCraft/Utils/EconLogger.java
+++ src/elxris/SpiceCraft/Utils/EconLogger.java
@@ -2,7 +2,10 @@ package elxris.SpiceCraft.Utils;
 
 import org.bukkit.entity.Player;
 
+import elxris.SpiceCraft.SpiceCraft;
+
 public class EconLogger extends Logger{
+    private static Boolean on;
     public EconLogger() {
         super("economy.logg");
     }
@@ -13,11 +16,20 @@ public class EconLogger extends Logger{
         logg(comando, p.getName(), concepto, datos, cantidad, dinero);
     }
     public void logg(String comando, String jugador, String concepto, String datos, int cantidad, String dinero){
+        if(!isOn()){
+            return;
+        }
         super.log(Fecha.formatoFecha(System.currentTimeMillis())+" ["+comando+"] "+jugador+
                 " "+concepto+
                 " "+datos+
                 ((cantidad != 0)?(" x "+cantidad):"")+
                 ": "+dinero);
         // [Shop] elxris buy carrots x 1 = 50 coins.
+    }
+    public Boolean isOn(){
+        if(on == null){
+            on = SpiceCraft.plugin().getConfig().getBoolean("econ.logg", true);
+        }
+        return on;
     }
 }
