3351822f16018a8a7b49208f76bdd038218b89fe
Christian Ceciliano <cristiancehi@gmail.com>
2015-03-09 10:31:32 -0600

    + Spigot 1.8.3 JSON Chat support

--- src/elxris/SpiceCraft/Utils/Chat.java
+++ src/elxris/SpiceCraft/Utils/Chat.java
@@ -1,18 +1,12 @@
 package elxris.SpiceCraft.Utils;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
 import java.util.IllegalFormatException;
 import java.util.List;
-import java.util.MissingFormatArgumentException;
 
-import org.bukkit.Bukkit;
+import net.md_5.bungee.chat.ComponentSerializer;
+
 import org.bukkit.entity.Player;
 
-import net.amoebaman.util.Reflection;
 import elxris.SpiceCraft.SpiceCraft;
 
 public class Chat {
@@ -38,78 +32,16 @@ public class Chat {
      * */
     
     private static void enviarJSON(Player p, String mensaje, Object... i){
-	    if(mensaje == null || mensaje == ""){
-	        return;
-	    }
-	    if(p == null){
-	        return;
-	    }
-	    try{
-	        String m = String.format(mensaje, i);
-	        Object handle = Reflection.getHandle(p);
-			Object connection = Reflection.getField(handle.getClass(), "playerConnection").get(handle);
-			Reflection.getMethod(connection.getClass(), "sendPacket", Reflection.getNMSClass("Packet")).invoke(connection, createChatPacket(m));
-	    }catch(IllegalFormatException e){
-	        p.sendMessage(mensaje);
-	        SpiceCraft.log("MISSING FORMAT ARGUMENT EXCEPTION \n" +
-	                "Caused by: "+mensaje);
-	    } catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
-			e.printStackTrace();
-		} catch (InstantiationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (NoSuchMethodException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SecurityException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
-	private static Constructor<?> nmsPacketPlayOutChatConstructor;
-    // The ChatSerializer's instance of Gson
- 	private static Object nmsChatSerializerGsonInstance;
- 	private static Method fromJsonMethod;
-
- 	private static Object createChatPacket(String json) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
- 		if(nmsChatSerializerGsonInstance == null){
- 			// Find the field and its value, completely bypassing obfuscation
- 			for(Field declaredField : Reflection.getNMSClass("ChatSerializer").getDeclaredFields()){
- 				if(Modifier.isFinal(declaredField.getModifiers()) && Modifier.isStatic(declaredField.getModifiers()) && declaredField.getType().getName().endsWith("Gson")){
- 					// We've found our field
- 					declaredField.setAccessible(true);
- 					nmsChatSerializerGsonInstance = declaredField.get(null);
- 					fromJsonMethod = nmsChatSerializerGsonInstance.getClass().getMethod("fromJson", String.class, Class.class);
- 					break;
- 				}
- 			}
- 		}
-
- 		// Since the method is so simple, and all the obfuscated methods have the same name, it's easier to reimplement 'IChatBaseComponent a(String)' than to reflectively call it
- 		// Of course, the implementation may change, but fuzzy matches might break with signature changes
- 		Object serializedChatComponent = fromJsonMethod.invoke(nmsChatSerializerGsonInstance, json, Reflection.getNMSClass("IChatBaseComponent"));
- 		
- 		if(nmsPacketPlayOutChatConstructor == null){
-			try {
-				nmsPacketPlayOutChatConstructor = Reflection.getNMSClass("PacketPlayOutChat").getDeclaredConstructor(Reflection.getNMSClass("IChatBaseComponent"));
-				nmsPacketPlayOutChatConstructor.setAccessible(true);
-			} catch (NoSuchMethodException e) {
-				SpiceCraft.log("Could not find Minecraft method or constructor.");
-			} catch (SecurityException e) {
-				SpiceCraft.log("Could not access constructor.");
-			}
-		}
- 		
- 		return nmsPacketPlayOutChatConstructor.newInstance(serializedChatComponent);
- 	}
- 	
+        if(mensaje == null || mensaje == ""){
+            return;
+        }
+        if(p == null){
+            return;
+        }
+        String m = String.format(mensaje, i);
+        p.spigot().sendMessage(ComponentSerializer.parse(m));
+    
+    }
     public static void mensaje(Player p, List<String> list, Object... i){
         enviar(p, Strings.parseList(list), i);
     }
